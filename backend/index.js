2c5d726171dd4f9bea658662dce7953ad0bd2918
Emmanuel López <lopezrodemmanuel@gmail.com>
2022-08-01 18:34:48 -0500

    MongoDB conectado

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,19 @@
-console.log('Desde NodeJS');
+import express from "express";
+import dotenv from "dotenv/config";
+import conectarBD from "./config/db.js";
+
+// Creación del servidor con Express
+const app = express();
+
+// Conexión de la base de datos de MongoDB
+conectarBD();
+
+app.use('/', (req, res) => {
+    res.send('Hola mundo!');
+});
+
+// Arrancar el servidor
+const port = process.env.PORT || 4000
+app.listen(port, () => {
+    console.log(`Servidor funcionando en el puerto ${port}`);
+});
